10b63714f4e35174c7f0507cbb2ac56c478e0233
Martin Hwang <developer.martinhwang@gmail.com>
2021-04-18 12:57:54 -0300

    update build

---
+++ build/precache-manifest.b037677c7ffc91d32e628155801552f5.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b3a9e2b7c7d5370a59ef9357858a50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca1fd77e6e632b9ceb5",
+    "url": "/static/css/2.883cdee8.chunk.css"
+  },
+  {
+    "revision": "e38fea38826adb211c52",
+    "url": "/static/css/main.be4168fc.chunk.css"
+  },
+  {
+    "revision": "6ca1fd77e6e632b9ceb5",
+    "url": "/static/js/2.e0cc04d0.chunk.js"
+  },
+  {
+    "revision": "c124f0c3d5365cf667f633ccca0131c9",
+    "url": "/static/js/2.e0cc04d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38fea38826adb211c52",
+    "url": "/static/js/main.8581c697.chunk.js"
+  },
+  {
+    "revision": "ebd1f17c2dea605a3ae6",
+    "url": "/static/js/runtime-main.e2b7e235.js"
+  },
+  {
+    "revision": "8885ce9860f3aed4c999dc1b9fb70633",
+    "url": "/static/media/Java-logo-icon.8885ce98.png"
+  },
+  {
+    "revision": "454e0b71b278982c0b8e3a2297fa705c",
+    "url": "/static/media/Martin-Hwang-Portfolio.454e0b71.pdf"
+  },
+  {
+    "revision": "02b93452f5df644b50052f51ff5bfc0d",
+    "url": "/static/media/Martin-Hwang-Resume.02b93452.pdf"
+  },
+  {
+    "revision": "480b2afa4e3dda4c18e118b6f6d45d27",
+    "url": "/static/media/Martin-Hwang-Transcript.480b2afa.pdf"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "664726beeda45028df6fcbc157730811",
+    "url": "/static/media/bluewallpaperbg.664726be.jpg"
+  },
+  {
+    "revision": "80c575ffc88788fa06111de24facceba",
+    "url": "/static/media/caricature404.80c575ff.png"
+  },
+  {
+    "revision": "5126fa88ce5be92b1635cbf9bc0bb82d",
+    "url": "/static/media/collaborationbg.5126fa88.jpg"
+  },
+  {
+    "revision": "ad1cf6279eb040b3ce39c6d6a328e52f",
+    "url": "/static/media/covid19CanadaMap.ad1cf627.png"
+  },
+  {
+    "revision": "83f78863a6cf2fab16d7f517af9045f3",
+    "url": "/static/media/covid19WorldMap.83f78863.png"
+  },
+  {
+    "revision": "7daa82251c2aefeba0c929b2bed917a8",
+    "url": "/static/media/css3.7daa8225.png"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/static/media/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/static/media/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/static/media/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/static/media/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/static/media/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/static/media/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/static/media/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/static/media/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/static/media/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/static/media/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/static/media/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/static/media/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/static/media/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/static/media/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "6abb977c1c8318c29905f44a17e3c4bc",
+    "url": "/static/media/figma-logo.6abb977c.png"
+  },
+  {
+    "revision": "c1497ba9e8475aeaf46a32f98127be64",
+    "url": "/static/media/gearbg.c1497ba9.png"
+  },
+  {
+    "revision": "e3b3ecc3b4a6876f2f8cc69de1258a27",
+    "url": "/static/media/html5.e3b3ecc3.png"
+  },
+  {
+    "revision": "804cbab1a9b4c8c3e362596c44be8301",
+    "url": "/static/media/iconmonstr-arrow-24.804cbab1.svg"
+  },
+  {
+    "revision": "d79851b53e7a7aa77e414a7d24b2b74f",
+    "url": "/static/media/javascript.d79851b5.png"
+  },
+  {
+    "revision": "47541c55f81a80906067b44d59dcf7f5",
+    "url": "/static/media/martin-photo.47541c55.jpg"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/static/media/material-ui.d2b1e190.png"
+  },
+  {
+    "revision": "80b8b8797da3e76516e182e688582057",
+    "url": "/static/media/mobilebg.80b8b879.jpg"
+  },
+  {
+    "revision": "521444a28d1ac7003ece0c1f030dcd21",
+    "url": "/static/media/node-js.521444a2.png"
+  },
+  {
+    "revision": "a431a33b24f73b2167a64ada71a29bdb",
+    "url": "/static/media/react.a431a33b.png"
+  },
+  {
+    "revision": "f4cc778b75b810234d4a5d2c17d37597",
+    "url": "/static/media/spring-framework.f4cc778b.png"
+  }
+]);
